3116e6a7d6dbdfa0b8b7ebf4921326499418d96a
JonathonAshworth <jonathonashworth@gmail.com>
2018-07-08 17:02:39 +1000

    implemented calculateCartCost and wrote some notes

--- src/app/pricingRules.js
+++ src/app/pricingRules.js
@@ -4,19 +4,19 @@
           real app you'd obviously store the rules in a database and write the code to
           apply them on the back-end. (and probably whip up a UI for sales people to
           adjust them)
-        - The 'priceList.classic * 2' bit of the solution isn't serializable,
-          so in a real case you'd have some extra back-end code to handle these sort of
-          cases
+        - The 'priceList.classic * 2' bit of the rule list isn't serializable,
+          so in a real case you'd have to structure the data differently and have some
+          back end code to handle these sort of cases
         - There's lots of different ways to do this, for example you could define a
           discount rather than a price, which might be more appropriate depending on
           what sort of price changes and pricing rules you want to implement in the future.
-        - I tend to comment-type non-obvious stuff like this when not using Flow/TS,
+        - I tend to comment-type non-obvious data structs like this when not using Flow/TS,
           just using arbitrary short-hand rather than any well-defined system. Hopefully
           it's fairly self-explanatory.
 */
 
 import { productPriceList } from './data.js'
-import { productSetAddRemove } from '../utils/productSet.js'
+import { productSetDifference, productSetContains } from '../utils/productSet.js'
 
 /*
     ProductSet: dict { string (productId): number (quantity) }
@@ -50,6 +50,23 @@ const pricingRules = [
 ]
 
 // cart: ProductSet
+// Cart cost is calculated by looping over the cart, identifying subsets of products
+// that match pricing rules, then tallying up the leftover products based on
+// their standard rate
 const calculateCartCost = (customerId, cart) => {
 
+    const _calculateCartCost = (remaining, accCost) => {
+        const ruleMatch = pricingRules.find(rule =>
+            rule.customerId === customerId
+            && productSetContains(remaining, rule.requirements)
+            && productSetContains(remaining, rule.productSet)
+        )
+        if (!ruleMatch) return accCost
+        return _calculateCartCost(
+            productSetDifference(remaining, ruleMatch.productSet),
+            accCost + ruleMatch.price
+        )
+    }
+
+    return _calculateCartCost(cart.slice(), 0)
 }
--- src/utils/productSet.js
+++ src/utils/productSet.js
@@ -1,23 +1,31 @@
 /*
     Notes
-        - This is just a single helper function for handling dict-like objects with
-          integer values (and a helper function for implementing that helper function, heh)
-        - It's used to add/remove items from a cart object, creating new properties and
-          tidying up empty ones in the process
-        - The objectFilter helper is hoisted, I like to have the export at the top for
-          readability purposes. In a real app with lots of code I'd likely have objectFilter
-          in it's own file for re-use, or more likely be using lodash's implementation
-          (pickBy? Can't remember...)
-
-    Examples:
-        - Adding
-            ({ classic: 3 }, classic, 2) => ({ classic: 5 })
-        - Removing
-            ({ standout: 1 }, standout, -1) => ({})
-        - Handling of negatives
-            ({ premium: 3 }, premium, -4) => ({})
+        - These are just some helper functions for handling dicts implemented as JS objects
+        - I copied over most of this code from another project of mine, and tweaked the
+          naming to be more specific to the domain. There's a trade-off when encoding
+          domain knowledge in identifiers, e.g. if I'd named it "dictAddRemove" etc
+          in my other project it'd be more re-usable, and I could just copy it straight
+          across with no changes, or release it as an npm module. However by putting in
+          some domain knowledge like "productSet" or "cart" it increases readability, and
+          your algorithms read closer to an English description of your requirements rather
+          than some abstract mathematical equation. I don't have a strong opinion either way
+          on this one, just thought the trade-off was interesting.
+        - In a real app with lots of code I'd likely have objectFilter in it's own file
+          for re-use, since it's not as related as the other functions and would likely be
+          re-used elsewhere (or more likely I'd be using lodash's pickBy...)
 */
 
+
+/*
+    Immutably add or remove items from a product set
+    Examples:
+        - Adding
+            ({ classic: 3 }, 'classic', 2) => ({ classic: 5 })
+        - Removing
+            ({ standout: 1 }, 'standout', -1) => ({})
+        - Handling of negatives
+            ({ premium: 3 }, 'premium', -4) => ({})
+*/
 export const productSetAddRemove(dict, productId, delta) => {
     const newDict = {
         ...dict,
@@ -30,6 +38,11 @@ export const productSetAddRemove(dict, productId, delta) => {
     }
 
     return objectFilter(dict, i => i !== 0)
+}
+
+// Performs a B diff A (aka B\A as per set theory) on a product set
+export const productSetDifference(b, a) => {
+
 }
 
 const objectFilter = (obj, filterFunc) =>
