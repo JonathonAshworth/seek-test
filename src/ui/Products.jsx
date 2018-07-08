f030fa1e13f1da3c325a26cb43579da1190466d9
JonathonAshworth <jonathonashworth@gmail.com>
2018-07-09 00:39:15 +1000

    added markup for products page

--- src/ui/Products.jsx
+++ src/ui/Products.jsx
@@ -1,9 +1,56 @@
+/*
+    Notes
+        - In a real app, the product types would obviously be pulled from a back-end
+          rather than hard-coded
+*/
+
 import React from 'react'
 
 import composeStyledComponent from '../utils/composeStyledComponent.jsx'
 
+
+const productTypes = [
+    { name: 'Classic', features: [true, false, false, false] },
+    { name: 'Standout', features: [true, true, true, false] },
+    { name: 'Premium', features: [true, true, true, true] },
+]
+
+const featureStrings = [
+    'Basics',
+    'Company Logo',
+    'Longer Presentation Text',
+    'Higher Visibility',
+]
+
 const Products = ({ styles }) =>
-    <div>Hello World!</div>
+    <div>
+        <div>
+            <h1>Classic Ad</h1>
+            <div>
+                <div>✔</div>
+                <p>Basics</p>
+            </div>
+            <div>
+                <div>✘</div>
+                <p>Company Logo</p>
+            </div>
+            <div>
+                <div>✘</div>
+                <p>Company Logo</p>
+            </div>
+        </div>
+        {productTypes.map(productType =>
+            <div>
+                <h1>{productType.name}</h1>
+                {featureStrings.map((featureString, index) =>
+                    <div>
+                        <div>{productType.features[index] ? '✔' : '✘'}</div>
+                        <p>{featureString}</p>
+                    </div>
+                )}
+            </div>
+        )}
+    </div>
 
 const styles = props => ({
     header: {
